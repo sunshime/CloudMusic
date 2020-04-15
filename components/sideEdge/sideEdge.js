4d1095a36024d6ff26da148ab7ea0161f285d540
lfj <liao_fengji@163.com>
2020-04-15 17:31:05 +0800

    实现发现页面静态

---
+++ components/sideEdge/sideEdge.js
@@ -0,0 +1,126 @@
+// components/sideEdge/sideEdge.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    setingList: [{
+        icon: "icon-wodexiaoxi",
+        name: '我的消息'
+      },
+      {
+        icon: "icon-wodehaoyou",
+        name: '我的好友'
+      },
+      {
+        icon: "icon-zhuye",
+        name: '个人主页'
+      },
+      {
+        icon: "icon-yifu",
+        name: '个性装扮'
+      }
+    ],
+    navList: [{
+        icon: "icon-yuyin",
+        name: '听歌识曲',
+        dec: '可识别其他app歌曲'
+      },
+      {
+        icon: "icon-huopiaotongxing",
+        name: '演出',
+        dec: ''
+      },
+      {
+        icon: "icon-gouwuche",
+        name: '商城',
+        dec: ''
+      },
+      {
+        icon: "icon-ren",
+        name: '附近的人',
+        dec: ''
+      },
+      {
+        icon: "icon-youxi",
+        name: '游戏推荐',
+        dec: ''
+      },
+      {
+        icon: "icon-icon--",
+        name: '口袋彩铃',
+        dec: ''
+      }
+    ],
+    operationList: [{
+        icon: "icon-yejianmoshi",
+        name: '夜间模式'
+      },
+      {
+        icon: "icon-shezhi",
+        name: '设置'
+      },
+      {
+        icon: "icon-tuichu",
+        name: '退出'
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,67 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+// pages/uplink/uplink.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    list: [{
+        name: '我的',
+        list: [
+          '时光年轮一圈圈转着',
+          '现在，他站在原地，回首望去，从前点点滴滴，琐琐碎碎',
+          '猛然发现，自己已走过了这么多',
+          '他想，如果再次遇到下雪天，他会毫不犹豫迎上去',
+          '因为那里，有旧时光味道。他仍在怀念，仍在留恋。怀念，白雪纷扬日子。'
+        ]
+      }, {
+        name: '发现',
+        list: [
+          '友情是每个人都应有的东西，而且相当的珍贵',
+          '在朋友之间，难免会产生分歧，让友情结冰',
+          '但我现在知道，友情上消融的冰雪是暖的。'
+        ]
+      },
+      {
+        name: '云村',
+        list: [
+          '有人安于某种生活，有人不能',
+          '因此能安于自已目前处境的不妨就如此生活下去，不能的只好努力另找出路',
+          '你无法断言哪里才是成功的，也无法肯定当自已到达了某一点之后，会不会快乐',
+          '有些人永远不会感到满足，他的快乐只建立在不断地追求与争取的过程之中',
+          '因此，他的目标不断地向远处推移。这种人的快乐可能少，但成就可能大。'
+        ]
+      },
+      {
+        name: '视频',
+        list: [
+          '加加加有人安于某种生活，有人不能',
+          '因此能安于自已目前处境的不妨就如此生活下去，不能的只好努力另找出路',
+          '你无法断言哪里才是成功的，也无法肯定当自已到达了某一点之后，会不会快乐',
+          '有些人永远不会感到满足，他的快乐只建立在不断地追求与争取的过程之中',
+          '因此，他的目标不断地向远处推移。这种人的快乐可能少，但成就可能大。'
+        ]
+      }
+    ],
+    curIndex: 1,
+    scrollLefts: 0,
+    duration: 300,
+    navItemWidth: 100, // 导航栏每一项的宽度
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  // 导航栏滑动
+  tabNav(e) {
+    let index = e.target.dataset.index;
+    this.setData({
+      curIndex: index,
+      scrollLefts: (index - 2) * this.data.navItemWidth
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  // 内容滑动
+  changeScroll(e) {
+    let index = e.detail.current;
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      curIndex: index,
+      scrollLefts: (index - 2) * this.data.navItemWidth
     })
   }
 })
